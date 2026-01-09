52f15f974c64f1ba1e94fb1b83618043b7f2d357
kaiwu <khoavu1831@gmail.com>
2026-01-09 15:37:14 +0700

    chore: delete function in service

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,35 +1,28 @@
 import { fetchApi } from "../api/http";
 import { TMDB_BASE_URL } from "../api/tmdb";
 
-export const getPopularMovies = () =>
+export const fetchPopularMovies = () =>
     fetchApi(`${TMDB_BASE_URL}/movie/popular?language=vi-VN&page=1`);
 
-export const getDetailsMovie = (id) =>
+export const fetchDetailsMovie = (id) =>
     fetchApi(`${TMDB_BASE_URL}/movie/${id}?language=vi-VN`);
 
-export const getSliderMovies = async () => {
-    const popularMovies = await getPopularMovies();
-    const topMovies = popularMovies.results.slice(0, 6);
-    const detailPromises = topMovies.map(movie => getDetailsMovie(movie.id));
-
-    return await Promise.all(detailPromises);
-}
-
-export const getTopMovies = async () => {
-    const popularMovies = await getPopularMovies();
-    const topMovies = popularMovies.results;
-    const detailPromises = topMovies.map(movie => getDetailsMovie(movie.id));
-
-    return await Promise.all(detailPromises);
-}
-
-export const getAnimeList = () =>
+export const fetchAnimes = () =>
     fetchApi(`${TMDB_BASE_URL}/discover/movie?language=vi-VN&with_original_language=ja&page=1`);
 
-export const getAnimeSliderData = async () => {
-    const animeList = await getAnimeList();
-    const topAnime = animeList.results.slice(0, 15);
-    const detailPromises = topAnime.map(movie => getDetailsMovie(movie.id));
+export const getTopMovies = async () => {
+    const popularMovies = await fetchPopularMovies();
+    const topMovies = popularMovies.results;
+    const detailPromises = topMovies.map(movie => fetchDetailsMovie(movie.id));
 
     return await Promise.all(detailPromises);
 }
+
+export const getAnimeSliderData = async () => {
+    const animeList = await fetchAnimes();
+    const topAnime = animeList.results.slice(0, 15);
+    const detailPromises = topAnime.map(movie => fetchDetailsMovie(movie.id));
+
+    return await Promise.all(detailPromises);
+}
+
